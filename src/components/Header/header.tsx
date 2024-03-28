0b34c3bde97a1b4ee0b3888bd20b33ac5afda0bc
kimpm78 <kimsoosang12@gmail.com>
2024-03-28 23:03:30 +0900

    login & logout logic fix

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -21,71 +21,56 @@ const Header: React.FC = () => {
 
   return (
     <>
-      {['xl'].map((expand) => (
-        <Navbar
-          key={expand}
-          expand={expand}
-          className="bg-color"
-          data-bs-theme="white"
-        >
-          <Container fluid>
-            <Navbar.Brand href="/">
-              <img
-                className="logo"
-                src="src/assets/Fox_Tech_Logo.svg"
-                alt="fox_tech_logo"
-              />
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
+      <Navbar expand="xl" className="bg-color" data-bs-theme="white">
+        <Container fluid>
+          <Navbar.Brand href="/">
+            <img
+              className="logo"
+              src="src/assets/Fox_Tech_Logo.svg"
+              alt="fox_tech_logo"
+            />
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="offcanvasNavbar" />
+          <Navbar.Offcanvas id="offcanvasNavbar" placement="end">
+            <Offcanvas.Header
+              closeButton
+              style={{ backgroundColor: '#f98351', height: '100px' }}
             >
-              <Offcanvas.Header
-                closeButton
-                style={{ backgroundColor: '#f98351', height: '100px' }}
-              >
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Fox Tech
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-5 nav-container">
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="#TrendingTopic">Trending</Nav.Link>
-                  <Nav.Link href="#Blog">Blog</Nav.Link>
-                  <Nav.Link href="#About">About</Nav.Link>
-                  <Nav.Link href="/SignUp">SignUp</Nav.Link>
-                </Nav>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    className="me-2 nav-input"
-                    placeholder="Search"
-                    aria-label="Search"
-                  />
-                  {isLoggedIn ? (
-                    <Button
-                      className="nav-submit"
-                      variant="dark"
-                      onClick={handleLogout}
-                    >
-                      ログアウト
-                    </Button>
-                  ) : (
-                    <Nav.Link href="/SignIn">
-                      <Button className="nav-submit" variant="dark">
-                        ログイン
-                      </Button>
-                    </Nav.Link>
-                  )}
-                </Form>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
-        </Navbar>
-      ))}
+              <Offcanvas.Title>Fox Tech</Offcanvas.Title>
+            </Offcanvas.Header>
+            <Offcanvas.Body>
+              <Nav className="justify-content-end flex-grow-1 pe-5 nav-container">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="#TrendingTopic">Trending</Nav.Link>
+                <Nav.Link href="#Blog">Blog</Nav.Link>
+                <Nav.Link href="#About">About</Nav.Link>
+                <Nav.Link href="/SignUp">SignUp</Nav.Link>
+              </Nav>
+              <Form className="d-flex">
+                <Form.Control
+                  type="search"
+                  className="me-2 nav-input"
+                  placeholder="Search"
+                  aria-label="Search"
+                />
+                {isLoggedIn ? (
+                  <Button
+                    className="nav-submit"
+                    variant="dark"
+                    onClick={handleLogout}
+                  >
+                    ログアウト
+                  </Button>
+                ) : (
+                  <Button className="nav-submit" href="/SignIn" variant="dark">
+                    ログイン
+                  </Button>
+                )}
+              </Form>
+            </Offcanvas.Body>
+          </Navbar.Offcanvas>
+        </Container>
+      </Navbar>
     </>
   );
 };
